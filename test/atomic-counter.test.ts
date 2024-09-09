ad2edcd73864fde3844481293e50d234f7d24620
Nicola Cremaschini <nicola.cremaschini@gmail.com>
2024-09-09 14:35:02 +0200

    Initial commit

---
+++ test/atomic-counter.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AtomicCounter from '../lib/atomic-counter-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/atomic-counter-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AtomicCounter.AtomicCounterStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
